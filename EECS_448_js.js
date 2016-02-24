34c364d1a4b175a30132155e63a664b56cfd94b5
Yarden Tamir <ytamir18@gmail.com>
2016-02-24 17:14:40 -0600

    Complete Exercise 1

---
+++ EECS_448_js.js
@@ -0,0 +1,17 @@
+function myFunc(a, b)
+{
+  if(a.length<8)
+  {
+    alert("Please update your passwords to both being over 8 characters");
+    return;
+  }
+  if(b.length<8)
+  {
+    alert("Please update your passwords to both being over 8 characters");
+    return;
+  }
+    if(a!=b)
+    {
+      alert("Please make sure that the values for your passwords are the same");
+    }
+}
